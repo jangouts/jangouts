25663a8345e9dfa6ee58e10017faff918551ae4b
Ancor Gonzalez Sosa <ancor@suse.de>
2015-05-06 10:18:03 +0200

    License headers

--- src/app/components/room/room.service.js
+++ src/app/components/room/room.service.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2015 SUSE Linux
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE.txt file for details.
+ */
+
 (function () {
   'use strict';
 
