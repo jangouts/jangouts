2bafa8c0ac06310d9fb5860e8ea66dab4529cee6
Imobach González Sosa <igonzalezsosa@suse.com>
2015-09-25 08:52:12 +0100

    Update nvm to version 0.26.1

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -37,7 +37,7 @@ sudo zypper -n in git
 # FIXME: in the future, packages for openSUSE should be used.
 if [ ! -d /home/vagrant/.nvm ]
 then
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
   source ~/.nvm/nvm.sh
   nvm install 0.12
   nvm alias default 0.12
