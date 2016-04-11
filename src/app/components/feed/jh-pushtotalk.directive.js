fd839144602321c6f0fb6d2ae320ff8e346c9f9b
sandr3j <47315303@opayq.com>
2016-04-12 01:24:19 +0200

    cleanup unused variable

--- src/app/components/feed/jh-pushtotalk.directive.js
+++ src/app/components/feed/jh-pushtotalk.directive.js
@@ -28,7 +28,6 @@
 
             var restrictedSupport = false;
             var origHandleKey = null;
-            var keyupDetected = false;
             var isCombination = false;
             var holding = true;
             var hooked = false;
