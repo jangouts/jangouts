5db2d9de547d6d56aaed90c633b5d98ce64f6219
Martin Garcia <newluxfero@gmail.com>
2016-06-04 20:46:32 +0200

    Add tests runner and tests for footerComponent

--- src/app/components/footer/jh-footer.directive.ts
+++ src/app/components/footer/jh-footer.directive.ts
@@ -4,14 +4,14 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
  */
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'jh-footer',
-  template: require('./jh-footer.html')
+  selector: "jh-footer",
+  template: require("./jh-footer.html")
 })
 export class FooterComponent {
-  version: string;
+  public version: string;
 
   constructor() {
     this.version = VERSION;
