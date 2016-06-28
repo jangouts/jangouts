0eed979642c22a812c69d0bc9d9b1330eb045930
Martin Garcia <newluxfero@gmail.com>
2016-06-28 16:25:29 +0200

    Update tests commands and fix some issues

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -35,3 +35,10 @@ interface Media {
 }
 
 declare function attachMediaStream(video: any, newVal: any): void;
+
+declare module 'zone.js/dist/zone' {
+    export var Zone; // this doesn't actually do anything just makes the compiler not complain about the empty module
+}
+declare module 'zone.js/dist/long-stack-trace-zone' {
+    export var Zone; // this doesn't actually do anything just makes the compiler not complain about the empty module
+}
