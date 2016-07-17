f09e3c98795d219c2a547201cb60d6db108756aa
Martin Garcia <newluxfero@gmail.com>
2016-07-17 17:05:42 +0200

    Update tests for connection-config

--- src/app/feed/shared/connection-config.model.spec.ts
+++ src/app/feed/shared/connection-config.model.spec.ts
@@ -20,19 +20,20 @@ describe("Service: ConnectionConfig", () => {
   });
 
   beforeEach(() => {
+    this.config = {
+      audio: true,
+      video: true,
+    };
+
     this.pluginHandle = {
-      send: jasmine.createSpy(": voidpluginHandle.send")
+      send: jasmine.createSpy("pluginHandle.send")
     };
   });
 
   it("should call pluginHandle.send on create", () => {
-    let config: IWanted = {
-      audio: true,
-      video: true,
-    };
     let cconfig = new ConnectionConfig(
       this.pluginHandle,
-      config,
+      this.config,
       {},
       undefined
     );
@@ -41,111 +42,97 @@ describe("Service: ConnectionConfig", () => {
     expect(this.pluginHandle.send).toHaveBeenCalled();
   });
 
-  it("should not be defined until confirm", () => {
-    let config: IWanted = {
-      audio: true,
-      video: true,
-    };
-    let cconfig: ConnectionConfig = new ConnectionConfig(
-      this.pluginHandle,
-      config,
-      {},
-      undefined
-    );
+  describe("#get", () => {
 
-    expect(cconfig.get()).not.toBeDefined();
+    it("should return undefined until confirm", () => {
+      let cconfig: ConnectionConfig = new ConnectionConfig(
+        this.pluginHandle,
+        this.config,
+        {},
+        undefined
+      );
 
-    cconfig.confirm();
+      expect(cconfig.get()).not.toBeDefined();
 
-    expect(cconfig.get()).toEqual(config);
+      cconfig.confirm();
+
+      expect(cconfig.get()).toEqual(this.config);
+    });
+
+    it("should show an error when pluginHandle raise an error", () => {
+      this.pluginHandle.send.and.callFake((options: any) => {
+        options.error();
+      });
+
+      let cconfig: ConnectionConfig = new ConnectionConfig(
+        this.pluginHandle,
+        this.config,
+        {},
+        undefined
+      );
+      spyOn(window.console, "error");
+
+      expect(cconfig.get()).not.toBeDefined();
+
+      cconfig.confirm();
+
+      expect(window.console.error).toHaveBeenCalled();
+    });
+
+    it("should call ok callback when confirm", () => {
+      this.pluginHandle.send.and.callFake((options: any) => {
+        options.success();
+      });
+
+      let ok: any = jasmine.createSpy("ok");
+
+      let cconfig: ConnectionConfig = new ConnectionConfig(
+        this.pluginHandle,
+        this.config,
+        {},
+        ok
+      );
+
+      expect(cconfig.get()).not.toBeDefined();
+
+      cconfig.confirm();
+
+      expect(ok).toHaveBeenCalled();
+    });
   });
 
-  it("should call console.error after pluginHandle.send calls error", () => {
-    let config: IWanted = {
-      audio: true,
-      video: true,
-    };
+  describe("#set", () => {
+    it("should set new options", () => {
+      this.pluginHandle.send.and.callFake((options: any) => {
+        options.success();
+      });
 
-    this.pluginHandle.send = jasmine.createSpy("pluginHandle.send").and.callFake((options: any) => {
-      options.error();
+      let ok: any = jasmine.createSpy("ok");
+      let newOk: any = jasmine.createSpy("newOk");
+
+      let cconfig: ConnectionConfig = new ConnectionConfig(
+        this.pluginHandle,
+        this.config,
+        {},
+        ok
+      );
+
+      expect(cconfig.get()).not.toBeDefined();
+
+      cconfig.set({
+        values: {
+          audio: true,
+          video: false
+        },
+        ok: ok
+      });
+      cconfig.confirm();
+      expect(ok).toHaveBeenCalled();
+
+      cconfig.set({ ok: newOk});
+      cconfig.confirm();
+      expect(newOk).not.toHaveBeenCalled();
     });
-
-    let cconfig: ConnectionConfig = new ConnectionConfig(
-      this.pluginHandle,
-      config,
-      {},
-      undefined
-    );
-    spyOn(window.console, "error");
-
-    expect(cconfig.get()).not.toBeDefined();
-
-    cconfig.confirm();
-
-    expect(window.console.error).toHaveBeenCalled();
-  });
-
-  it("should call ok callback when confirm", () => {
-    let config: IWanted = {
-      audio: true,
-      video: true,
-    };
-
-    this.pluginHandle.send = jasmine.createSpy("pluginHandle.send").and.callFake((options: any) => {
-      options.success();
-    });
-
-    let ok: any = jasmine.createSpy("ok");
-
-    let cconfig: ConnectionConfig = new ConnectionConfig(
-      this.pluginHandle,
-      config,
-      {},
-      ok
-    );
-
-    expect(cconfig.get()).not.toBeDefined();
-
-    cconfig.confirm();
-
-    expect(ok).toHaveBeenCalled();
-  });
-
-  it("should not call new ok callback when pluginHandle success for all calls", () => {
-    let config: IWanted = {
-      audio: true,
-      video: true,
-    };
-
-    this.pluginHandle.send = jasmine.createSpy("pluginHandle.send").and.callFake((options: any) => {
-      options.success();
-    });
-
-    let ok: any = jasmine.createSpy("ok");
-    let newOk: any = jasmine.createSpy("newOk");
-
-    let cconfig: ConnectionConfig = new ConnectionConfig(
-      this.pluginHandle,
-      config,
-      {},
-      ok
-    );
-
-    expect(cconfig.get()).not.toBeDefined();
-
-    cconfig.set({
-      values: {
-        audio: true,
-        video: false
-      },
-      ok: ok
-    });
-    cconfig.confirm();
-    expect(ok).toHaveBeenCalled();
-
-    cconfig.set({ ok: newOk});
-    cconfig.confirm();
-    expect(newOk).not.toHaveBeenCalled();
   });
 
 });
