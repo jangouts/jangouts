ca2af06556f63efdf31f5611360f0f1538fd4bf5
Mohsin Ammar <mohsini172@gmail.com>
2017-03-09 22:10:24 +0500

    change the code style and removed the rootScope function

--- src/app/index.js
+++ src/app/index.js
@@ -69,21 +69,19 @@ angular.module('janusHangouts', ['ngAnimate', 'ngCookies', 'ngTouch',
       return $delegate;
     });
   })
-  .run(function($rootScope,$http, jhConfig) {
-    //global function to replace the placeholders
-    $rootScope.replacePlaceholder = function(value){
-      return value.replace("%{hostname}",window.location.host)
+  .run(function($http, jhConfig) {
+    //function to replace the placeholders
+    function replacePlaceholder(value) {
+      return value.replace("%{hostname}", window.location.host);
     }
-    //function ends here
-
     var request = new XMLHttpRequest();
     request.open('GET', 'config.json', false);
     request.send(null);
     if (request.status === 200) {
       var config = JSON.parse(request.responseText);
-      angular.forEach(config, function(value, key) {
+      angular.forEach(config, function (value, key) {
         //assigning config value with replaced value of placeholder
-         jhConfig[key] = $rootScope.replacePlaceholder(value);
+        jhConfig[key] = replacePlaceholder(value);
       });
     } else {
       console.warn('No configuration found');
