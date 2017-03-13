5ba1728f9eb9d35262178d2e221c7746cf40f907
code_master5 <lauhny.bimalk@gmail.com>
2017-03-14 00:05:19 +0530

    Topbar Name-Change Feature

--- src/app/components/feed/jh-main-feed-info.directive.js
+++ src/app/components/feed/jh-main-feed-info.directive.js
@@ -11,9 +11,9 @@
   angular.module('janusHangouts')
     .directive('jhMainFeedInfo', jhMainFeedInfoDirective);
 
-  jhMainFeedInfoDirective.$inject = ['FeedsService'];
+  jhMainFeedInfoDirective.$inject = ['FeedsService', '$state'];
 
-  function jhMainFeedInfoDirective(FeedsService) {
+  function jhMainFeedInfoDirective(FeedsService, $state) {
     return {
       restrict: 'EA',
       templateUrl: 'app/components/feed/jh-main-feed-info.html',
@@ -27,11 +27,76 @@
       /* jshint: validthis */
       var vm = this;
       vm.name = name;
+      vm.makeNameEditable = makeNameEditable;
+      vm.leaveEditingMode = leaveEditingMode;
+      vm.editing = false; /* Name Editing mode */
+      vm.isEditing = isEditing;
+      vm.updateName = updateName;
+      vm.newName = null;
+      vm.keyDetect = keyDetect;
 
       function name() {
         var feed = FeedsService.findMain();
         var name = (feed !== null) ? feed.display : "";
         return name;
+      }
+
+      /**
+       * Detects the key pressed by user
+       * @param {Number} - numeric keycode
+       */
+      function keyDetect(event) {
+        // if keypressed is 'Enter'
+        // then update the name to the current value in input box
+        console.log("This is detected keycode", event);
+        if (event.keyCode === 13) {
+          vm.updateName();
+          vm.leaveEditingMode();
+        // if keypressed is 'Escape'
+        // then leave the editing mode without changing name
+        } else if (event.keyCode === 27) {
+          vm.leaveEditingMode();
+        }
+      }
+      
+      /**
+       * Toggles the name editing mode
+       *
+       */
+      function leaveEditingMode() {
+        vm.editing = false;
+        vm.newName = null;
+      }
+      
+      /**
+       * Allows editing the name on double-click
+       * 
+       */
+      function makeNameEditable() {
+        vm.editing = true;
+      }
+      
+      /**
+       * Checks if the user is currently editing name or not
+       *
+       * @returns {boolean}
+       */
+      function isEditing() {
+        return vm.editing;
+      }
+      
+      /**
+       * Update the name
+       * 
+       */
+      function updateName() {
+        if (vm.newName) {
+          FeedsService
+            .findMain()
+            .updateDisplay(vm.newName);
+          $state.go('room', {user: vm.newName}, {notify: false});
+        }
+        this.leaveEditingMode();
       }
     }
   }
