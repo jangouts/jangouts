31f7a73826294e94459447b6e6215327fe3c86e9
code_master5 <lauhny.bimalk@gmail.com>
2017-03-19 15:05:16 +0530

    Remove Extra Spaces and logs

--- src/app/components/feed/auto-focus.directive.js
+++ src/app/components/feed/auto-focus.directive.js
@@ -4,8 +4,6 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
  * 
- * This module implements conditional autofocus functionality
- * for input
  */
 
 'use strict';
