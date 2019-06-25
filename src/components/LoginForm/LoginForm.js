38fc040f808fadd70bbbb070d657069c4d4d1be3
Imobach González Sosa <igonzalezsosa@suse.com>
2019-06-25 17:27:44 +0100

    Extend LoginForm component to fetch rooms list from the server

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,8 +1,20 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import janusApi from '../../janus-api';
 import './LoginForm.css';
 
+function roomOptions(rooms) {
+  return rooms.map((r) => (
+    <option key={r.id} value={r.id}>{r.description}</option>
+  ));
+}
+
 function LoginForm() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    janusApi.getRooms().then((r) => setRooms(r));
+  }, []);
+
   return (
     <form className="LoginForm">
       <div className="form-row">
@@ -11,7 +23,9 @@ function LoginForm() {
       </div>
       <div className="form-row">
         <label htmlFor="room">Room</label>
-        <select id="room" name="room"></select>
+        <select id="room" name="room">
+          {roomOptions(rooms)}
+        </select>
       </div>
       <div className="form-row">
         <input type="submit" value="Sign in" />
