0de23fa1d0cc62c7ba5c7a0cb0f2c4d6e6e8c92d
Imobach González Sosa <igonzalezsosa@suse.com>
2019-06-26 09:58:05 +0100

    Add events services to the Janus API

---
+++ src/janus-api/events-service.test.js
@@ -0,0 +1,24 @@
+/**
+* Copyright (c) [2019] SUSE Linux
+*
+* This software may be modified and distributed under the terms
+* of the MIT license.  See the LICENSE.txt file for details.
+*/
+
+import { createEventsService } from './events-service';
+
+describe('#emitEvent', () => {
+  test('emits an event with the configured room and user', () => {
+    let eventsService = createEventsService();
+    const subscriber = jest.fn();
+    const event = { type: "request", data: "payload" };
+    eventsService.setRoom("room1");
+    eventsService.setUser("user1");
+    eventsService.getEventsSubject().subscribe(subscriber);
+
+    eventsService.emitEvent(event);
+    expect(subscriber).toHaveBeenCalledWith(
+      { ...event, user: "user1", room: "room1" }
+    );
+  });
+});
