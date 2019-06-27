b4638c6a5c25a27067af1b5f61f4bfb6564f6deb
Imobach González Sosa <igonzalezsosa@suse.com>
2019-06-27 13:04:37 +0100

    Add a mechanism to initialize the janus-api module

--- src/janus-api/index.js
+++ src/janus-api/index.js
@@ -10,16 +10,58 @@
  *
  * @todo Read configuration.
  */
-import server from './server';
+import { createRoomService } from './room-service';
+import { createFeedsService } from './feeds-service';
+import { createLogService } from './log-service';
+import { createEventsService } from './events-service';
+import { createDataChannelService } from './data-channel-service';
+import { createActionService } from './action-service';
+import { createFeed } from './models/feed';
 
 // TODO: get this value from the configuration
-const defaultUrl = 'ws://localhost:8188/janus';
+const DEFAULT_URL = 'ws://localhost:8188/janus';
 
 export default (function () {
-  var that = {};
+  var that = {
+    dataChannelService: null,
+    eventsService: null,
+    feedsService: null,
+    logService: null,
+    roomService: null,
+    actionService: null
+  };
 
-  that.server = server({janusServer: defaultUrl});
-  that.getRooms = () => that.server.getRooms();
+  that.setup = function(options = {}) {
+    let defaultUrl = options.serverUrl || DEFAULT_URL;
+
+    that.eventsService = createEventsService();
+    that.eventsService.getEventsSubject().subscribe((e) =>console.log("events subject", e)
+    );
+    that.feedsService = createFeedsService();
+    that.logService = createLogService();
+    that.dataChannelService = createDataChannelService(that.feedsService, that.logService);
+
+    const createFeedFactory = createFeed(that.dataChannelService, that.eventsService);
+    that.actionService = createActionService(
+      that.feedsService,
+      that.logService,
+      that.dataChannelService,
+      createFeedFactory
+    );
+    that.roomService = createRoomService(
+      { janusServer: defaultUrl },
+      that.feedsService,
+      that.dataChannelService,
+      that.eventsService,
+      that.actionService
+    );
+  };
+
+  that.getRooms = () => that.roomService.getRooms();
+  that.enterRoom = (room, username) => {
+    that.roomService.setRoom(room);
+    return that.roomService.enter(username);
+  };
 
   return that;
 })();
