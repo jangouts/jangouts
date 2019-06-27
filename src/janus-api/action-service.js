2396f59e2af4ec19b60c6dd8213974a2a34ca101
Imobach González Sosa <igonzalezsosa@suse.com>
2019-06-27 20:52:39 +0100

    Add missing action-service module

---
+++ src/janus-api/action-service.js
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) [2019] SUSE Linux
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE.txt file for details.
+ */
+
+import { createLogEntry } from './models/log-entry';
+import { createFeedFactory } from './models/feed';
+
+export const createActionService = (
+  feedsService,
+  logService,
+  dataChannelService,
+  eventsService
+) => {
+  let that = {};
+
+  const createFeed = createFeedFactory(dataChannelService, eventsService);
+
+  that.enterRoom = function(feedId, display, connection) {
+    var feed = createFeed({
+      display: display,
+      connection: connection,
+      id: feedId,
+      isPublisher: true
+    });
+    feedsService.add(feed, { main: true });
+  };
+
+  that.leaveRoom = function() {
+    feedsService.allFeeds().forEach(f => that.destroyFeed(f.id));
+  };
+
+  that.publishScreen = function(feedId, display, connection) {
+    var feed = createFeed({
+      display: display,
+      connection: connection,
+      id: feedId,
+      isPublisher: true,
+      isLocalScreen: true
+    });
+    feedsService.add(feed);
+    // Log the event
+    var entry = createLogEntry('publishScreen');
+    logService.add(entry);
+  };
+
+  that.remoteJoin = function(feedId, display, connection) {
+    var feed = createFeed({
+      display: display,
+      connection: connection,
+      id: feedId,
+      isPublisher: false
+    });
+    feedsService.add(feed);
+    // Log the event
+    var entry = createLogEntry('newRemoteFeed', { feed: feed });
+    logService.add(entry);
+  };
+
+  that.destroyFeed = function(feedId) {
+    var feed = feedsService.find(feedId);
+    if (feed === null) {
+      return;
+    }
+    window.setTimeout(function() {
+      // TODO: is the timeout really needed?
+      feed.disconnect();
+      feedsService.destroy(feedId);
+    });
+    // Log the event
+    var entry = createLogEntry('destroyFeed', { feed: feed });
+    logService.add(entry);
+  };
+
+  that.unpublishFeed = function(feedId) {
+    var feed = feedsService.find(feedId);
+    if (feed === null) {
+      return;
+    }
+    window.setTimeout(function() {
+      // TODO: is the timeout really needed?
+      feed.disconnect();
+      feedsService.destroy(feedId);
+    });
+  };
+
+  that.ignoreFeed = function(feedId) {
+    var feed = feedsService.find(feedId);
+    if (feed === null) {
+      return;
+    }
+    feed.ignore();
+    // Log the event
+    var entry = createLogEntry('ignoreFeed', { feed: feed });
+    logService.add(entry);
+  };
+
+  that.stopIgnoringFeed = function(feedId, connection) {
+    var feed = feedsService.find(feedId);
+    if (feed === null) {
+      return;
+    }
+    feed.stopIgnoring(connection);
+    // Log the event
+    var entry = createLogEntry('stopIgnoringFeed', { feed: feed });
+    logService.add(entry);
+  };
+
+  that.writeChatMessage = function(text) {
+    var entry = createLogEntry('chatMsg', {
+      feed: feedsService.findMain(),
+      text: text
+    });
+    if (entry.hasText()) {
+      logService.add(entry);
+      dataChannelService.sendChatMessage(text);
+    }
+  };
+
+  that.toggleChannel = function(type, feed) {
+    // If no feed is provided, we are muting ourselves
+    if (!feed) {
+      feed = feedsService.findMain();
+      if (!feed) {
+        return;
+      }
+    }
+    if (!feed.isPublisher) {
+      // Log the event
+      var entry = createLogEntry('muteRequest', {
+        source: feedsService.findMain(),
+        target: feed
+      });
+      logService.add(entry);
+    }
+    if (feed.isEnabled(type)) {
+      var callback = null;
+      // If we are muting the main feed (the only publisher that can be
+      // actually muted) raise a signal
+      if (type === 'audio' && feed.isPublisher) {
+        // TODO: dispatch an event?
+        //  callback = function() { $rootScope.$broadcast('muted.byUser'); };
+      }
+      feed.setEnabledChannel(type, false, { after: callback });
+    } else {
+      feed.setEnabledChannel(type, true);
+    }
+  };
+
+  /**
+   * Disable or enable audio or video for the main feed
+   * @param {channel type} type
+   * @param {Boolean} boolval
+   */
+  that.setMedia = function(type, boolval) {
+    var feed = feedsService.findMain();
+    if (!feed) {
+      return;
+    }
+
+    feed.setEnabledChannel(type, boolval);
+  };
+
+  return that;
+};
