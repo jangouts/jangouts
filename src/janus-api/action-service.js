44122d3e7580a6d3561b507368bcbe46e93081a3
David Díaz González <dgonzalez@suse.de>
2019-06-30 16:49:34 +0100

    Fix copyright headers

--- src/janus-api/action-service.js
+++ src/janus-api/action-service.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) [2019] SUSE Linux
+ * Copyright (c) [2015-2019] SUSE Linux
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
