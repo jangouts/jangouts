67deb741ffd83e130a468a172fc6f669b2dc2000
David Díaz González <dgonzalez@suse.de>
2020-03-22 19:52:52 +0000

    Allow to turn on/off the thumbnail mode

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,19 +6,29 @@
  */
 
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Button from '../Button';
 import { actionCreators as roomActions } from '../../state/ducks/room';
 import { actionCreators as participantActions } from '../../state/ducks/participants';
 import { GiExitDoor } from 'react-icons/gi';
-import { MdScreenShare } from 'react-icons/md';
+import { MdScreenShare, MdPhoto } from 'react-icons/md';
 
 import './Sidebar.css';
 
 // TODO: Button seems no longer necessary. Use simpler approach
 function Sidebar() {
+  const thumbnailMode = useSelector((state) => state.room.thumbnailMode);
+
   return (
     <div className="Sidebar">
       <Button className="red" />
+      <Button
+        action={roomActions.toggleThumbnailMode}
+        title={`Turn thumbnail mode ${thumbnailMode ? 'off' : 'on'}`}
+        className={thumbnailMode ? 'red' : ''}>
+        <MdPhoto />
+      </Button>
       <Button title="Share screen" action={participantActions.startScreenSharing}>
         <MdScreenShare />
       </Button>
