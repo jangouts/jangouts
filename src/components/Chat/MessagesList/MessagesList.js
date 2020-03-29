586498d2ba34b1724e330d706ee79b637e03e33f
Ancor Gonzalez Sosa <ancor@suse.de>
2020-03-30 01:24:11 +0200

    WIP: chat scrolling

--- src/components/Chat/MessagesList/MessagesList.js
+++ src/components/Chat/MessagesList/MessagesList.js
@@ -4,13 +4,22 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
  */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import Message from '../Message';
 
 function MessagesList() {
   const messages = useSelector((state) => state.messages);
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  // FIXME: if the message contains an <img> tag, this can be triggered
+  // before the image is loaded.
+  useEffect(scrollToBottom);
 
   return (
     <div className="MessageList">
@@ -19,6 +28,7 @@ function MessagesList() {
           <Message key={index} {...m} />
         ))}
       </ul>
+      <div ref={messagesEndRef} />
     </div>
   );
 }
