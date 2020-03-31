9d900ac18c8b36bcfe4c9932669f9449549510fc
Ancor Gonzalez Sosa <ancor@suse.de>
2020-03-31 15:04:29 +0200

    Auto-scroll in the chat

--- src/components/Chat/MessagesList/MessagesList.js
+++ src/components/Chat/MessagesList/MessagesList.js
@@ -4,17 +4,42 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
  */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import Message from '../Message';
 
 function MessagesList() {
   const messages = useSelector((state) => state.messages);
+  const wrapperRef = useRef(null);
+  const messagesRef = useRef(null);
+
+  const mustScroll = (message) => {
+    // FIXME: instead of using this threshold, we could verify whether the
+    // previous message is right at the end of the viewport
+    const threshold = 100;
+    const wrapper = wrapperRef.current;
+
+    const wrapperViewBottom = wrapper.scrollTop + wrapper.clientHeight;
+    const msgTop = message.offsetTop;
+    const msgBottom = msgTop + message.clientHeight;
+
+    return msgBottom > wrapperViewBottom && msgTop <= wrapperViewBottom + threshold;
+  };
+
+  const updateScroll = () => {
+    const lastMsg = messagesRef.current.lastChild;
+
+    if (!lastMsg || !mustScroll(lastMsg)) return;
+
+    lastMsg.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  };
+
+  useEffect(updateScroll);
 
   return (
-    <div className="MessageList">
-      <ul>
+    <div className="MessageList" ref={wrapperRef}>
+      <ul ref={messagesRef}>
         {messages.map((m, index) => (
           <Message key={index} {...m} />
         ))}
