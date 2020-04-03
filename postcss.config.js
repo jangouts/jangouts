649af9baff11435d8c429cfaa06c0759c31ccafc
David Díaz González <dgonzalez@suse.de>
2020-04-03 01:14:26 +0100

    Add postcss-import plugin

--- postcss.config.js
+++ postcss.config.js
@@ -8,6 +8,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
