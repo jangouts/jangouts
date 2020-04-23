bed2f28b2ee23ed74f804013624f8a9fbe191e9d
David Díaz González <dgonzalez@suse.de>
2020-04-23 22:35:16 +0100

    Update the Room component to use a layout

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) [2015-2019] SUSE Linux
+ * Copyright (c) [2015-2020] SUSE Linux
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
@@ -9,16 +9,9 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Redirect } from 'react-router';
-
 import { actionCreators as roomActions } from '../../state/ducks/room';
 
-import Header from '../Header';
-import Sidebar from '../Sidebar';
-import Speaker from '../Speaker';
-import Participants from '../Participants';
-import Chat from '../Chat';
-
-import './Room.css';
+import { Classic as RoomLayout } from '../layouts';
 
 const randomUsername = () => `user_${Math.floor(Math.random() * 1000)}`;
 
@@ -39,17 +32,7 @@ function Room({ location }) {
     return <Redirect to="/" />;
   }
 
-  return (
-    <div className="Room">
-      <Header />
-      <Sidebar />
-      <Chat />
-      <div className="shared-panel">
-        <Speaker />
-        <Participants />
-      </div>
-    </div>
-  );
+  return <RoomLayout />;
 }
 
 export default Room;
