a9f5a2bcf0e016d1d030cf187cc45a4011c0a645
David Díaz González <dgonzalez@suse.de>
2020-04-26 19:20:15 +0100

    Relocate and simplify the Room actions
    Those global actions were living spread between the no longer available
    Sidebar and Button components.

--- src/components/Room/Actions/LogOut.test.js
+++ src/components/Room/Actions/LogOut.test.js
@@ -1,14 +1,14 @@
 /**
- * Copyright (c) [2015-2019] SUSE Linux
+ * Copyright (c) [2020] SUSE Linux
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
  */
 
 import React from 'react';
-import { renderWithRedux } from '../../setupTests';
-import Button from './Button';
+import LogOut from './LogOut';
+import { renderWithRedux } from '../../../setupTests';
 
 it('renders without crashing', () => {
-  renderWithRedux(<Button />);
+  renderWithRedux(<LogOut />);
 });
