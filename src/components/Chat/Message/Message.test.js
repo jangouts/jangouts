e6a0beb5b85e1872b32277ccd0559ddb096c5cc6
Ancor Gonzalez Sosa <ancor@suse.de>
2020-04-26 19:59:28 +0200

    Some tests for rendering of messages

--- src/components/Chat/Message/Message.test.js
+++ src/components/Chat/Message/Message.test.js
@@ -9,7 +9,10 @@ import React from 'react';
 import Message from './Message';
 import { render } from '@testing-library/react';
 
-const message = {
+// TODO: I was not able to test the emoji matcher. Not even after checking how it is done here:
+// https://github.com/milesj/interweave/blob/master/packages/emoji/tests/Interweave.test.tsx
+
+const plainMessage = {
   type: 'chatMsg',
   text: () => 'Hi Jane!',
   content: {
@@ -21,23 +24,64 @@ const message = {
 };
 
 it('renders without crashing', () => {
-  render(<Message {...message} />);
+  render(<Message {...plainMessage} />);
 });
 
 it('renders the user name', () => {
-  const { getByText } = render(<Message {...message} />);
+  const { getByText } = render(<Message {...plainMessage} />);
 
   expect(getByText('John')).toBeInTheDocument();
 });
 
 it('renders the message', () => {
-  const { getByText } = render(<Message {...message} />);
+  const { getByText } = render(<Message {...plainMessage} />);
 
   expect(getByText('Hi Jane!')).toBeInTheDocument();
 });
 
 it('renders the time in HH:MM format', () => {
-  const { getByText } = render(<Message {...message} />);
+  const { getByText } = render(<Message {...plainMessage} />);
 
   expect(getByText('18:01')).toBeInTheDocument();
+});
+
+const htmlMessage = {
+  type: 'chatMsg',
+  text: () => 'This is <b>bold</b> and this an <script>window.alert();</script> injection!',
+  content: {
+    feed: {
+      display: 'John'
+    }
+  },
+  timestamp: '2020-02-10T18:02:58.439Z'
+};
+
+it('filters out the dangerous HTML markup', () => {
+  const { getByTestId } = render(<Message {...htmlMessage} />);
+
+  expect(getByTestId('message')).not.toContainHTML('<script>');
+  expect(getByTestId('message')).not.toContainHTML('window.alert');
+});
+
+it('keeps the acceptable HTML markup', () => {
+  const { getByTestId } = render(<Message {...htmlMessage} />);
+
+  expect(getByTestId('message')).toContainHTML('<b>bold</b>');
+});
+
+const imgMessage = {
+  type: 'chatMsg',
+  text: () => 'Link to a logo http://www.google.com/logo.png',
+  content: {
+    feed: {
+      display: 'John'
+    }
+  },
+  timestamp: '2020-02-10T18:02:58.439Z'
+};
+
+it('renders images inline', () => {
+  const { getByTestId } = render(<Message {...imgMessage} />);
+
+  expect(getByTestId('message')).toContainHTML('<img src="http://www.google.com/logo.png"');
 });
