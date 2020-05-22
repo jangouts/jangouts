bd2fbbec1ef423ae848e8c43738c35728c5419b4
Imobach González Sosa <igonzalezsosa@suse.com>
2020-05-22 09:37:25 +0100

    Add timeouts to events

--- src/state/ducks/notifications.js
+++ src/state/ducks/notifications.js
@@ -10,22 +10,47 @@
  *
  * @todo Remember which notifications should be 'silenced'.
  */
-import { fromEvent as notificationFromEvent } from '../../utils/notifications';
+import { fromEvent as notificationFromEvent, UserNotification } from '../../utils/notifications';
 
 const NOTIFICATION_SHOW = 'jangouts/notification/SHOW';
 const NOTIFICATION_CLOSE = 'jangouts/notification/CLOSE';
+const DEFAULT_TIMEOUT = 5000;
 
 /**
  * Notify that a given event has happened.
  *
+ * @param [Object] event - Event to notify
+ * @param [Number] timeout - Timeout (in miliseconds)
  * @see notifications
  */
-const notifyEvent = (event) => (dispatch) => {
+const notifyEvent = (event, timeout) => (dispatch) => {
   const notification = notificationFromEvent(event);
-  if (!notification) {
-    return null;
+  if (notification) {
+    dispatch(notify(notification, timeout));
   }
+};
+
+/**
+ * Notify a message.
+ *
+ * @param [String] message - Message text
+ * @param [String] severity - Message's severity
+ * @param [Number] timeout - Timeout (in miliseconds)
+ */
+const notifyMessage = (text, severity, timeout) => (dispatch) => {
+  const notification = new UserNotification(text, severity);
+  dispatch(notify(notification, timeout));
+};
+
+/**
+ * Action to display a notification (and hide it after the timeout)
+ *
+ * @param [UserNotification] notification - Notification
+ * @param [Number] timeout - Timeout (in miliseconds)
+ */
+const notify = (notification, timeout = DEFAULT_TIMEOUT) => (dispatch) => {
   dispatch(show(notification));
+  setTimeout(() => dispatch(close(notification.id)), timeout);
 };
 
 const show = (notification) => ({
@@ -40,6 +65,7 @@ const close = (id) => ({
 
 const actionCreators = {
   notifyEvent,
+  notifyMessage,
   show,
   close
 };
--- src/state/ducks/notifications.test.js
+++ src/state/ducks/notifications.test.js
@@ -7,6 +7,11 @@
 
 import { UserNotification } from '../../utils/notifications';
 import reducer, { actionCreators } from './notifications';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 const notification = new UserNotification('You have been muted.');
 const other_notification = new UserNotification('Nobody is listening!');
@@ -20,5 +25,62 @@ describe('reducer', () => {
   it('handles NOTIFICATION_CLOSE', () => {
     const action = actionCreators.close(notification.id);
     expect(reducer([other_notification, notification], action)).toEqual([other_notification]);
+  });
+});
+
+describe('action creators', () => {
+  jest.useFakeTimers();
+
+  const event = {
+    type: 'muted',
+    data: { cause: 'request', source: { id: 1, display: 'Jane' } }
+  };
+
+  describe('notifyEvent', () => {
+    it('adds a message about an event and removes it after a timeout', () => {
+      const store = mockStore({ notifications: [] });
+      store.dispatch(actionCreators.notifyEvent(event));
+
+      expect(store.getActions()).toEqual([
+        expect.objectContaining({
+          payload: expect.objectContaining({
+            notification: expect.objectContaining({
+              text: 'You have been muted by Jane.'
+            })
+          }),
+          type: 'jangouts/notification/SHOW'
+        })
+      ]);
+
+      jest.runAllTimers();
+      expect(store.getActions()).toEqual([
+        expect.objectContaining({ type: 'jangouts/notification/SHOW' }),
+        expect.objectContaining({ type: 'jangouts/notification/CLOSE' })
+      ]);
+    });
+  });
+
+  describe('notifyMessage', () => {
+    it('adds a message and removes it after a timeout', () => {
+      const store = mockStore({ notifications: [] });
+      store.dispatch(actionCreators.notifyMessage('You have been muted.'));
+
+      expect(store.getActions()).toEqual([
+        expect.objectContaining({
+          payload: expect.objectContaining({
+            notification: expect.objectContaining({
+              text: 'You have been muted.'
+            })
+          }),
+          type: 'jangouts/notification/SHOW'
+        })
+      ]);
+
+      jest.runAllTimers();
+      expect(store.getActions()).toEqual([
+        expect.objectContaining({ type: 'jangouts/notification/SHOW' }),
+        expect.objectContaining({ type: 'jangouts/notification/CLOSE' })
+      ]);
+    });
   });
 });
