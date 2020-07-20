bd0892b522b664e9ac612c717ad9a51e68078fb7
David Díaz González <dgonzalez@suse.de>
2020-07-20 16:41:50 +0100

    Do not use an IIFE for StreamService

--- src/utils/streams-service.js
+++ src/utils/streams-service.js
@@ -5,32 +5,30 @@
  * of the MIT license.  See the LICENSE.txt file for details.
  */
 
-export default (function() {
-  let streams = {};
-  let that = {};
-
+const streams = {};
+const StreamService = {
   /**
    * @returns {MediaStream} gets stream associated to the given id or null if not found
    */
-  that.get = (id) => {
+  get: (id) => {
     return streams[id] || null;
-  };
+  },
 
   /**
    * Registers a stream, associating it to an id.
    * @param {Integer} id - id under which is registered
    * @param {MediaStream} stream
    */
-  that.add = (id, stream) => {
+  add: (id, stream) => {
     streams[id] = stream;
-  };
+  },
 
   /**
    * Unregisters the stream associated to the given id.
    */
-  that.delete = (id) => {
+  delete: (id) => {
     delete streams[id];
-  };
+  }
+};
 
-  return that;
-})();
+export default StreamService;
