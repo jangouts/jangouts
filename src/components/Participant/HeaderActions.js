fbfe1b1c8b602aa48576222e3aa03fcf7e20fec2
Ancor Gonzalez Sosa <ancor@suse.de>
2023-02-03 15:16:45 +0100

    Buttons at the header to toggle local audio and video

---
+++ src/components/Participant/HeaderActions.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) [2023] SUSE Linux
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE.txt file for details.
+ */
+
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectors } from '../../state/ducks/participants';
+import { ToggleAudio, ToggleVideo } from './Actions';
+
+function HeaderActions() {
+  const participant = useSelector((state) => selectors.localParticipant(state.participants));
+
+  if (!participant) {
+    return null;
+  }
+
+  const actionsProps = {
+    participantId: participant.id,
+    showLabel: false,
+    className: 'p-0.5 rounded'
+  };
+
+  return [
+    <ToggleAudio key="toggle-audio-header" {...actionsProps} />,
+    <ToggleVideo key="toggle-video-header" video={participant.video} {...actionsProps} />
+  ];
+}
+
+export default HeaderActions;
