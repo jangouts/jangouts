00a7ff4365fb203a7e48fda66974c5e25b46e382
David Díaz González <dgonzalez@suse.de>
2023-11-07 10:20:29 +0000

    Stop using taliwindcss in src/layouts/*

--- src/components/layouts/Classic/index.js
+++ src/components/layouts/Classic/index.js
@@ -17,8 +17,6 @@ import ChatToggler from '../../Chat/Toggler';
 import ParticipantActions from '../../Participant/HeaderActions';
 import Notifications from '../../Notifications';
 
-import { classNames } from '../../../utils/common';
-
 const START = "start";
 const END = "end";
 
@@ -56,35 +54,26 @@ function Classic() {
   };
 
   return (
-    <div className="w-screen h-screen bg-primary-dark border-b-1 border-primary-dark">
-      <div className="h-full padding-b-4 flex flex-col bg-gray-100">
-        <div className="px-4 pt-2 pb-1 text-white font-bold border-b-4 border-secondary bg-primary-dark">
+    <div className="full-viewport">
+      <div className="wrapper">
+        <div className="header-wrapper">
           <Header>
             <ParticipantActions />
             <ChatToggler />
           </Header>
         </div>
-        <div className="flex-1 transition-all pt-2 overflow-hidden grid gap-2 grid-rows-6 grid-cols-2 lg:grid-cols-3 sm:grid-flow-col">
-          <Notifications
-            className="w-full absolute z-50 flex flex-col items-center"
+        <div className="content-wrapper">
+          <ChatColumn />
+          <Notifications className="notifications-wrapper"
           />
-          <div
-            className={classNames(
-              'flex p-1 justify-center overflow-hidden row-span-2 col-span-2 sm:row-span-3 sm:col-span-1 lg:col-span-2',
-              !showChat && 'sm:row-span-6'
-            )}>
+          <div className="speaker-wrapper">
             <Speaker />
           </div>
-          <div
-            className={classNames(
-              'overflow-y-auto row-span-2 col-span-2 border-t sm:row-span-3 sm:col-span-1 lg:col-span-2 sm:border-t-0',
-              !showChat && 'row-span-4 sm:row-span-6'
-            )}>
+          <div className="participants-wrapper">
             <Participants />
           </div>
-          <ChatColumn />
         </div>
-        <div className="bg-primary-dark border-t-2 border-secondary text-xs text-white text-center py-1">
+        <div className="footer">
           Powered by <a target="_blank" rel="noreferrer" href="https://github.com/jangouts/jangouts" className="underline">Jangouts</a>
         </div>
       </div>
